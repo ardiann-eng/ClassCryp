35332945aa97d8b9e3178811def47ca76fbbe798
ardianndr21 <41235211-ardianndr21@users.noreply.replit.com>
2025-04-07 11:37:06 +0000

    Restored to '8995201aee8864fb1de6d9d3036f0cc8c0403f99'
    Replit-Restored-To: 8995201aee8864fb1de6d9d3036f0cc8c0403f99

--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Render the React application
-createRoot(document.getElementById("root")!).render(<App />);
